f564224ed02edcceedcfddb94c7d17bdc6ee3fc7
seokzin <seokzin95@gmail.com>
2022-01-24 15:10:30 +0900

    fix: Remove deprecated extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,26 +4,28 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    'react-app',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   overrides: [
     {
-      files: ["*.js", "*.jsx"],
+      files: ['*.js', '*.jsx'],
       rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
+        '@typescript-eslint/explicit-function-return-type': 'off',
       },
     },
   ],
   rules: {},
-  ignorePatterns: ["**/*.html"],
+  ignorePatterns: ['**/*.html'],
 };
