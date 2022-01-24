195a1f552a28544fd99deed0427c4c11683ce8b3
seokzin <seokzin95@gmail.com>
2022-01-24 13:17:40 +0900

    feat: Set firebase javascript SDK

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { app } from "./firebase";
+
+console.log(app);
 
 ReactDOM.render(<App />, document.getElementById("root"));
