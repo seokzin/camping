d1f98555855282e416ba69cbb76730a751a01583
seokzin <seokzin95@gmail.com>
2022-01-26 14:24:00 +0900

    design: Set mobile size layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { Card, List, Player, SearchBar, Spinner } from './components';
 import Router from './routes';
@@ -7,7 +8,7 @@ import { NormalizeStyle, GlobalStyle } from './styles';
 
 const App = () => {
   return (
-    <>
+    <Layout>
       <NormalizeStyle />
       <GlobalStyle />
       <h1>App</h1>
@@ -19,8 +20,23 @@ const App = () => {
       <Spinner />
 
       <Router />
-    </>
+    </Layout>
   );
 };
+
+const Layout = styled.div`
+  box-sizing: border-box;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  padding: 1rem;
+  width: 375px;
+  height: 812px;
+
+  background-color: white;
+  border-radius: 1rem;
+`;
 
 export default App;
