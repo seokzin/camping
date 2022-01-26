c332b319ad90077bad17c2a8defaae60ccaf4de5
seokzin <seokzin95@gmail.com>
2022-01-26 10:27:26 +0900

    feat: Add directory templates

---
+++ src/store/playlist/actions.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const actions = () => {
+  return <div></div>;
+};
+
+export default actions;
---
+++ src/store/playlist/types.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const types = () => {
+  return <div></div>;
+};
+
+export default types;
