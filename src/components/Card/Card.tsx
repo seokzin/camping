ed248b49653763bc8195cb98da44683140c1e548
seokzin <seokzin95@gmail.com>
2022-01-27 23:59:22 +0900

    design: Card hover effect

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,22 +5,34 @@ const Card = (data: any) => {
   const movie = {
     videoId: data.data.id.videoId,
     title: data.data.snippet.title,
+    channelTitle: data.data.snippet.channelTitle,
     thumbnail: data.data.snippet.thumbnails.medium.url,
   };
 
   return (
     <Layout>
       <Image src={movie.thumbnail} />
-      {/* <p>{movie.videoId}</p> */}
       <Title>{movie.title}</Title>
+      <ChannelTitle>{movie.channelTitle}</ChannelTitle>
     </Layout>
   );
 };
 
-const Layout = styled.div``;
+const Layout = styled.div`
+  margin-bottom: 2rem;
 
-const Title = styled.h1`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const Title = styled.h2`
+  margin-top: 0.3rem;
+
   font-size: ${({ theme }) => theme.fontSize.md};
+  color: ${({ theme }) => theme.mode.mainText};
 
   overflow: hidden;
   display: -webkit-box;
@@ -28,8 +40,17 @@ const Title = styled.h1`
   -webkit-box-orient: vertical;
 `;
 
+const ChannelTitle = styled.h3`
+  margin-top: 0.2rem;
+
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  color: ${({ theme }) => theme.mode.subText};
+`;
+
 const Image = styled.img`
   width: 100%;
+  height: 12rem;
+  border-radius: 0.5rem;
 `;
 
 export default Card;
