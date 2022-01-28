44626933026070c36dca7472c712300d4506dc86
seokzin <seokzin95@gmail.com>
2022-01-28 23:49:35 +0900

    feat: Add player-minimize layout

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -8,22 +8,27 @@ const duration = dataVideoDetail.items[0].contentDetails.duration;
 const Player = () => {
   const video = {
     title: data.title,
-    channel: data.channelId,
+    channel: data.channelTitle,
     thumbnail: data.thumbnails.default.url,
     duration,
   };
 
   return (
     <Layout>
-      <p>{video.title}</p>
-      <p>{video.channel}</p>
-      <p>{video.thumbnail}</p>
-      <p>{video.duration}</p>
+      <Image src={video.thumbnail} />
+
+      <div>
+        <Title>{video.title}</Title>
+        <ChannelTitle>{video.channel}</ChannelTitle>
+        <p>{video.duration}</p>
+      </div>
     </Layout>
   );
 };
 
 const Layout = styled.div`
+  padding: 0 1rem;
+
   display: flex;
   justify-content: center;
   align-items: center;
@@ -37,6 +42,29 @@ const Layout = styled.div`
   /* background-color: ${({ theme }) => theme.mode.mainColor}; */
   background-color: ${({ theme }) => theme.mode.subColor};
   z-index: 100;
+`;
+
+const Title = styled.h1`
+  font-size: ${({ theme }) => theme.fontSize.md};
+  color: ${({ theme }) => theme.mode.mainText};
+
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+`;
+
+const ChannelTitle = styled.h3`
+  margin-top: 0.2rem;
+
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  color: ${({ theme }) => theme.mode.subText};
+`;
+
+const Image = styled.img`
+  height: 3rem;
+  border-radius: 0.25rem;
+  margin-right: 0.5rem;
 `;
 
 export default Player;
