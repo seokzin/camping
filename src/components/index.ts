0db302b941c7a993a1bcab9cda5cd26a6de72d8b
seokzin <seokzin95@gmail.com>
2022-01-30 02:25:52 +0900

    refac: Combine searchbar + searchresult components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,6 @@
 export { default as Card } from './Card';
 export { default as List } from './List';
 export { default as Player } from './Player';
-export { default as SearchBar } from './SearchBar';
-export { default as SearchResult } from './SearchResult';
 export { default as Spinner } from './Spinner';
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import { SearchBar, SearchResult } from '@/components';
+import youtube from '@/services/youtube';
+import { SearchIcon } from '@/assets/icons';
 
-// TODO: isLoading, isData
 const Search = () => {
+  const [term, setTerm] = useState('');
+
+  const onChange = (e: any) => {
+    setTerm(e.target.value);
+  };
+
+  const onSubmit = async (e: any) => {
+    if (e.type === 'click' || e.key === 'Enter') {
+      const response = await youtube.get('/search', {
+        params: {
+          q: term,
+        },
+      });
+      console.log(response.data);
+    }
+  };
+
   return (
     <>
-      <SearchBar />
-      <SearchResult />
+      <Layout>
+        <SearchInput onChange={onChange} onKeyPress={onSubmit} value={term} />
+        <SearchIcon width={26} height={26} onClick={onSubmit} />
+      </Layout>
     </>
   );
 };
+
+const Layout = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  width: 100%;
+  margin-bottom: 1rem;
+
+  border-bottom: 1px solid gray;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  height: 2rem;
+  border: none;
+  background-color: transparent;
+  font-size: ${({ theme }) => theme.fontSize.lg};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+
+  :focus {
+    outline: none;
+  }
+`;
 
 export default Search;
