d48b076e7f8c895c3863e7c976a303f6f994db97
seokzin <seokzin95@gmail.com>
2022-01-30 17:48:57 +0900

    feat: Remove youtube api default params

--- src/services/youtube.ts
+++ src/services/youtube.ts
@@ -3,8 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
-    part: 'snippet',
-    maxResults: 30,
     key: process.env.REACT_APP_YOUTUBE_API_KEY,
   },
 });
