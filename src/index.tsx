c1ade1635e7a6545d67e2fad6d5e1ac2ba84d134
seokzin <seokzin95@gmail.com>
2022-02-03 15:59:07 +0900

    refac: Remove unnecessary codes

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/App';
-import { app } from '@/firebase';
-
-console.log(app);
 
 ReactDOM.render(
   <BrowserRouter>
