14fafc54c50df496191ff49f48d78d7a5a71588c
seokzin <seokzin95@gmail.com>
2022-02-03 23:38:55 +0900

    rename: Change redux store dir name

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import store from '@/store/player';
+import { store } from '@/features/store';
 import { Provider } from 'react-redux';
 
 import App from '@/App';
