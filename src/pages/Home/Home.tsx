b73d19e8432fbd9ac9832b05a2d13bf985c55f3f
seokzin <seokzin95@gmail.com>
2022-02-03 15:47:07 +0900

    fix: Show youtube api response
    - API response가 Card의 요소들과 매칭되지 않아 오류 발생
    - Category id list : https://gist.github.com/dgp/1b24bf2961521bd75d6c

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+import youtube from '@/services/youtube';
 import { Card } from '@/components/';
 import { dataSearch } from '@/assets/data';
 
 const items = dataSearch.items;
 
 const Home = () => {
+  const [data, setData] = useState<any[]>([]);
+
+  const fetchData = async () => {
+    const response = await youtube.get('/videos', {
+      params: {
+        part: 'snippet',
+        // q: term,
+        chart: 'mostPopular',
+        maxResults: 10,
+        regionCode: 'KR',
+        videoCategoryId: '10', // Music
+      },
+    });
+    setData(response.data.items);
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(data);
+  }, []);
+
   return (
     <>
       <Title>인기 동영상</Title>
 
-      {items.map((data, index) => (
-        <Card data={data} key={index} />
+      {data?.map((item, index) => (
+        <Card data={item} key={index} />
       ))}
     </>
   );
