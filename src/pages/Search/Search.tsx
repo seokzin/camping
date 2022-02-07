3ddca3bcddd1a265578a1a56250f94c2deed54fd
seokzin <seokzin95@gmail.com>
2022-02-07 22:44:06 +0900

    feat: Show search results card

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,33 +1,50 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import { Card } from '@/components/';
 import youtube from '@/services/youtube';
 import { SearchIcon } from '@/assets/icons';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+  const [data, setData] = useState<any[]>([]);
 
   const onChange = (e: any) => {
     setTerm(e.target.value);
   };
 
+  // TODO : useEffect?
   const onSubmit = async (e: any) => {
-    if (e.type === 'click' || e.key === 'Enter') {
+    if (term && (e.type === 'click' || e.key === 'Enter')) {
       const response = await youtube.get('/search', {
         params: {
+          part: 'snippet',
           q: term,
+          maxResults: 10,
+          regionCode: 'KR',
+          // videoCategoryId: '10', // FIX ME: Not working
         },
       });
-      console.log(response.data);
+      setData(response.data.items);
+      console.log(data);
     }
   };
 
   return (
     <>
       <Layout>
-        <SearchInput onChange={onChange} onKeyPress={onSubmit} value={term} />
+        <SearchInput
+          onChange={onChange}
+          onKeyPress={onSubmit}
+          value={term}
+          placeholder='검색어를 입력해주세요.'
+        />
         <SearchIcon width={26} height={26} onClick={onSubmit} />
       </Layout>
+
+      {data?.map((item, index) => (
+        <Card data={item} key={index} />
+      ))}
     </>
   );
 };
@@ -49,10 +66,13 @@ const SearchInput = styled.input`
   border: none;
   background-color: transparent;
   font-size: ${({ theme }) => theme.fontSize.lg};
-  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  font-weight: ${({ theme }) => theme.fontWeight.semiBold};
 
   :focus {
     outline: none;
+  }
+  ::placeholder {
+    color: ${({ theme }) => theme.mode.subText};
   }
 `;
 
