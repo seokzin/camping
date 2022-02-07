5524190f38ed28e71aedb323a15b467cd8d9c383
seokzin <seokzin95@gmail.com>
2022-02-07 16:22:49 +0900

    feat: Mapping playlist store data

--- src/pages/PlayList/PlayList.tsx
+++ src/pages/PlayList/PlayList.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+import { RootState } from '@/features/store';
+import { PlayListCard } from '@/components';
+
 const PlayList = () => {
-  return <div>PlayList Page</div>;
+  const videos = useSelector((state: RootState) => state.videos.value);
+
+  return (
+    <>
+      <h1>Playlist</h1>
+
+      <div>
+        {videos.map((name) => (
+          <PlayListCard name={name} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default PlayList;
