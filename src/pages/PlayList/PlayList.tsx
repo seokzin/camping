850636339cf4ecba1ae65508440e07900a281700
seokzin <seokzin95@gmail.com>
2022-02-08 04:43:32 +0900

    wip: Show playlist card

--- src/pages/PlayList/PlayList.tsx
+++ src/pages/PlayList/PlayList.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useSelector } from 'react-redux';
 import { RootState } from '@/features/store';
-import { PlayListCard } from '@/components';
+import { PlayListCard, Spinner } from '@/components';
 
 const PlayList = () => {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
   const videos = useSelector((state: RootState) => state.videos.videos);
+
+  const fetchData = async () => {
+    const response = await youtube.get('/videos', {
+      params: {
+        part: 'snippet,contentDetails',
+        // q: term,
+        chart: 'mostPopular',
+        maxResults: 10,
+        regionCode: 'KR',
+        videoCategoryId: '10', // Music
+      },
+    });
+    setData(response.data.items);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    console.log(videos);
+  }, [videos]);
 
   return (
     <>
@@ -17,12 +39,12 @@ const PlayList = () => {
         ))}
       </div>
 
-      <iframe
+      {/* <iframe
         id='player'
         width='640'
         height='360'
         src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
-      ></iframe>
+      ></iframe> */}
     </>
   );
 };
