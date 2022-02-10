7d3aeb14626f03942973b161b90585962b1c44e3
seokzin <seokzin95@gmail.com>
2022-02-11 04:01:49 +0900

    feat: Add state interfaces

--- src/features/videoSlice.ts
+++ src/features/videoSlice.ts
@@ -1,30 +1,55 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import youtube from '@/services/youtube';
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 
 interface VideoState {
-  nowVideo: string;
-  videos: string[];
+  // nowVideo: Video;
+  nowVideo: any;
+  videos: Video[];
+}
+
+interface Video {
+  id: string;
+  title: string;
+  channelTitle: string;
+  thumbnail: string;
+  duration: string;
+  bookmark: boolean;
 }
 
 const initialState: VideoState = {
-  nowVideo: '',
+  nowVideo: undefined,
   videos: [],
 };
+
+export const getVideos = createAsyncThunk('GET_VIDEOS', async () => {
+  const response = await youtube.get('/videos', {
+    params: {
+      part: 'snippet,contentDetails',
+      chart: 'mostPopular',
+      maxResults: 10,
+      regionCode: 'KR',
+      videoCategoryId: '10', // Music
+    },
+  });
+
+  return response;
+});
 
 export const videoSlice = createSlice({
   name: 'videos',
   initialState,
   reducers: {
-    addVideo: (state, action: PayloadAction<string>) => {
-      console.log('add');
+    playVideo: (state, action: PayloadAction<Video>) => {
+      state.nowVideo = action.payload;
+    },
+    addVideo: (state, action: PayloadAction<Video>) => {
       state.videos.push(action.payload);
     },
-    removeVideo: (state, action: PayloadAction<string>) => {
-      console.log('remove');
-      state.videos.filter((item) => item !== action.payload);
-      // state.videos.splice(state.videos.findIndex((item) => item === action.payload), 1);
+    removeVideo: (state, action: PayloadAction<Video>) => {
+      state.videos = state.videos.filter((item) => item.id !== action.payload.id);
     },
   },
 });
 
-export const { addVideo, removeVideo } = videoSlice.actions;
+export const { playVideo, addVideo, removeVideo } = videoSlice.actions;
 export default videoSlice.reducer;
