560f8780cbef469d1fc39c1ac39a26f87b168da8
seokzin <seokzin95@gmail.com>
2022-02-13 23:50:08 +0900

    refac: Remove list component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Card } from './Card';
-export { default as List } from './List';
 export { default as Player } from './Player';
 export { default as Spinner } from './Spinner';
 export { default as Header } from './Header';
