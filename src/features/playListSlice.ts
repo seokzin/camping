e2b1e47743d358d5bd43cc038323f1e638fff835
seokzin <seokzin95@gmail.com>
2022-02-23 15:57:58 +0900

    feat: Separate playListSlice

---
+++ src/features/playListSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction, current } from '@reduxjs/toolkit';
+import type { RootState } from '@/app/rootReducer';
+import { Video } from './store.types';
+
+interface playListState {
+  playList: Video[];
+}
+
+const initialState: playListState = {
+  playList: [],
+};
+
+export const playListSlice = createSlice({
+  name: 'playList',
+  initialState,
+  reducers: {
+    addVideo: (state, { payload }: PayloadAction<Video>) => {
+      if (!current(state.playList).some((video) => video.id === payload.id)) {
+        state.playList.push(payload);
+      }
+    },
+    removeVideo: (state, { payload }: PayloadAction<Video>) => {
+      state.playList = state.playList.filter((item) => item.id !== payload.id);
+    },
+  },
+});
+
+export const { addVideo, removeVideo } = playListSlice.actions;
+export const selectPlayList = (state: RootState) => state.playList;
+export default playListSlice.reducer;
