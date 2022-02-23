68be2daf511c5d457cfb5f5721a13d7da7fd7d94
seokzin <seokzin95@gmail.com>
2022-02-23 16:58:30 +0900

    feat: Declare youtube API response interface

--- src/features/store.types.ts
+++ src/features/store.types.ts
@@ -6,3 +6,82 @@ export interface Video {
   duration: string;
   bookmark: boolean;
 }
+
+export interface YoutubeResponse {
+  kind: string;
+  etag: string;
+  regionCode?: string;
+  nextPageToken: string;
+  pageInfo: PageInfo;
+  items: Item[];
+}
+
+interface Item {
+  kind: string;
+  etag: string;
+  id: SearchResponseId | string;
+  snippet: Snippet;
+  ContentDetails: ContentDetails;
+}
+
+interface SearchResponseId {
+  kind: string;
+  videoId: string;
+}
+
+interface Snippet {
+  publishedAt: string;
+  publishTime?: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  tags?: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: Localized;
+}
+
+interface Localized {
+  title: string;
+  description: string;
+}
+
+interface Thumbnails {
+  default: Thumbnail;
+  medium: Thumbnail;
+  high: Thumbnail;
+  standard: Thumbnail;
+  maxres: Thumbnail;
+}
+
+interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface ContentDetails {
+  duration: string;
+  dimension: string;
+  definition: string;
+  caption: string;
+  licensedContent: boolean;
+  regionRestriction: RegionRestriction;
+  contentRating: ContentRating;
+  projection: string;
+}
+
+interface RegionRestriction {
+  allowed: string[];
+}
+
+interface ContentRating {
+  [key: string]: string;
+}
+
+interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
