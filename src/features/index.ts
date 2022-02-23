bab18e39a51c39984260c6bf5ba1196404412949
seokzin <seokzin95@gmail.com>
2022-02-23 17:53:30 +0900

    feat: Indexing features dir

---
+++ src/features/index.ts
@@ -0,0 +1,4 @@
+export { default as playingVideoSlice } from './playingVideoSlice';
+export { default as playListSlice } from './playListSlice';
+export { default as popularListSlice } from './popularListSlice';
+export { default as searchListSlice } from './searchListSlice';
