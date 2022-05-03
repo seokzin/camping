002a9df9eeaac415108f4a9a5164e26fecb0fd2d
seokzin <seokzin95@gmail.com>
2022-05-03 10:57:23 +0900

    refac: Spinner 스타일 파일 분리

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,16 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
+import { Layout } from './Spinner.styled';
 
 const Spinner = () => {
   return <Layout>Loading...</Layout>;
 };
-
-const Layout = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  height: 35rem;
-`;
 
 export default Spinner;
