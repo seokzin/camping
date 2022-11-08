dc557b92ab88db9fdac998da7d29a3eb8890cc77
seokzin <seokzin95@gmail.com>
2022-11-08 15:17:11 +0900

    feat: 로고 벡터 및 폰트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
-      <head />
+      <head>
+        <title>Camping</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Camping with Music" />
+        <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
+      </head>
       <body>{children}</body>
     </html>
   )
