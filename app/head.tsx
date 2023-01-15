8ab442a616dc70b509e6806874286f8876ebe653
seokzin <seokzin95@gmail.com>
2023-01-15 22:52:38 +0900

    refac: head 메타파일 분리

---
+++ app/head.tsx
@@ -0,0 +1,16 @@
+export default function Head() {
+  return (
+    <>
+      <title>Camping</title>
+      <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Camping with Music" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Camping" />
+      <meta property="og:url" content="https://seokzin-camping.vercel.app/" />
+      <meta property="og:image" content="images/opengraph.jpg" />
+      <meta property="og:description" content="Camping with music ⛺" />
+    </>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,18 +10,7 @@ import RootStyleLayout from './styleLayout'
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
-      <head>
-        <title>Camping</title>
-        <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="description" content="Camping with Music" />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Camping" />
-        <meta property="og:url" content="https://seokzin-camping.vercel.app/" />
-        <meta property="og:image" content="images/opengraph.jpg" />
-        <meta property="og:description" content="Camping with music ⛺" />
-      </head>
+      <head />
 
       <body>
         <RootStyleLayout>
