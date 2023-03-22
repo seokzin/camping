9a22bdb97652862e6f8df6f7e07d961ad33be89d
seokzin <seokzin95@gmail.com>
2023-03-22 14:39:10 +0900

    feat: 글로벌 스타일 보강

--- styles/global.ts
+++ styles/global.ts
@@ -1,21 +1,17 @@
 import { createGlobalStyle } from 'styled-components'
 
+import reset from './reset'
+
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Pretendard Variable';
-    font-style: normal;
-    font-weight: 45 920;
-    src: local('Pretendard Variable'), url('/fonts/PretendardVariable.woff2') format('woff2-variations');
-    font-display: swap;
-  }
-  
+  ${reset}
+
   *, *::before, *::after {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-    font-family: 'Pretendard Variable', sans-serif;
+
+    /* font-family: Pretendard, sans-serif; */
     line-height: 1.3;
-    letter-spacing: -0.02rem;
   }
 `
 
