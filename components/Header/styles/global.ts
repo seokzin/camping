4ad245870b46e6134ff4db0d2f72b78cbc289462
seokzin <seokzin95@gmail.com>
2023-10-30 20:45:39 +0900

    feat: Header 만듦

---
+++ components/Header/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+import reset from './reset'
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+
+    /* font-family: Pretendard, sans-serif; */
+    line-height: 1.3;
+  }
+`
+
+export default GlobalStyle
